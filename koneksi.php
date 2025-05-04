cded7fd0bd16ed2f356bdf1aa8a439b4a612285f
azwaralfaizzz <azwaralfaizzz@gmail.com>
2025-05-04 20:50:28 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "db_pengaduan";
+$host = "sql103.ezyro.com";
+$user = "ezyro_38899257";
+$pass = "15debd7";
+$db = "ezyro_38899257_pengaduan";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
